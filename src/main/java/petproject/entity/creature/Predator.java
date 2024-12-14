2f604279e427c037c5a6ed6f935577957f8b23af
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2024-12-14 17:26:50 +0300

    Predator can attack Herbivore

--- src/main/java/petproject/entity/creature/Predator.java
+++ src/main/java/petproject/entity/creature/Predator.java
@@ -2,9 +2,12 @@ package petproject.entity.creature;
 
 import petproject.BreadthFirstSearch;
 import petproject.Coordinates;
+import petproject.entity.Entity;
 import petproject.entity.Grass;
 
 import java.util.List;
+
+import static petproject.Main.simulation;
 
 public class Predator extends Creature {
     int attack;
@@ -12,9 +15,19 @@ public class Predator extends Creature {
 
     @Override
     public void makeMove() {
-        List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Grass());
-        Coordinates coordinatesToMove = wayToObject.get(speed - 1);
-        this.setCoordinates(coordinatesToMove);
+        List<Coordinates> wayToObject = breadthFirstSearch.findClosestObjectCoordinates(this.getCoordinates(), new Herbivore());
+
+        if (wayToObject.size() == 1) {
+            Coordinates coordinatesOfHerbivore = wayToObject.get(0);
+            Entity[][] map = simulation.getMap().getMap();
+            Herbivore herbivore = (Herbivore) map[coordinatesOfHerbivore.getX()][coordinatesOfHerbivore.getY()];
+            int herbivoreHp = herbivore.getHp();
+            herbivore.setHp(herbivoreHp - attack);
+        }
+        else {
+            Coordinates coordinatesToMove = wayToObject.get(speed - 1);
+            setCoordinates(coordinatesToMove);
+        }
     }
 
     @Override
