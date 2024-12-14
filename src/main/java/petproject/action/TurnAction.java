e182dbf886514e5f7ff009580915cc89ef8c3a55
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2024-12-14 16:32:54 +0300

    Add logic in TurnAction

--- src/main/java/petproject/action/TurnAction.java
+++ src/main/java/petproject/action/TurnAction.java
@@ -1,5 +1,23 @@
 package petproject.action;
 
-public abstract class TurnAction extends Action {
+import petproject.Map;
+import petproject.entity.Entity;
+import petproject.entity.creature.Creature;
 
+public class TurnAction extends Action {
+    /**
+     Processes the moves of all creatures on the map.
+     @param mapObj The map object which contains all the entities, including the creatures.
+     */
+    public void processCreatureMoves(Map mapObj){
+        Entity[][] map = mapObj.getMap();
+
+        for (Entity[] entities : map) {
+            for (Entity entity : entities) {
+                if(entity instanceof Creature creature){
+                    creature.makeMove();
+                }
+            }
+        }
+    }
 }
